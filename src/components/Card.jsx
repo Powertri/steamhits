d23402577edd927d1017a0bff008516970defb51
Logon Aluno <nathi-borges@hotmail.com>
2023-09-05 21:36:29 -0300

    fix component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,18 @@
+"use client"
+
+import { HeartIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
+
 export default function Card({filme}){
+    const [favorito, setFavorito] = useState(true)
+
     return(
         <div id="card" className='flex flex-col w-40 justify-center items-center m-2'>
+        { favorito ? 
+            <HeartIcon onClick={() => setFavorito(false)} className="h-6 w-6 text-rose-600 cursor-pointer " />
+        :
+            <HeartIcon onClick={() => setFavorito(true)}  className="h-6 w-6 text-slate-100 cursor-pointer" />
+        }
         <img className='rounded' src={filme.poster} alt="" />
         <span className='font-bold text-center line-clamp-1 '>{filme.titulo}</span>
         <div>
